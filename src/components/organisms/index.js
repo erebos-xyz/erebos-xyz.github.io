6a1495f1583d8a1caf17cef71f548efcfd8ace6e
petschna <petschna@edu.htl-villach.at>
2020-07-02 13:12:56 +0200

    Add NavbarGlobal to index.js
    The NavbarGlobal has
    been added to the index.js.

--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -2,6 +2,7 @@
 // Import all components to export them for easy access from parent components
 //import FooterGlobal from "./FooterGlobal";
 //import NavbarGlobal from "./NavbarGlobal";
+import NavbarGlobal from "./NavbarGlobal";
 import AboutPage from "./SectionAbout";
 import SectionMotd from "./SectionMotd";
 import SectionSharingan from "./SectionSharingan";
@@ -10,6 +11,7 @@ import SectionCommunity from "./SectionCommunity";
 export {
   //NavbarGlobal,
   //FooterGlobal,
+  NavbarGlobal,
   AboutPage,
   SectionMotd,
   SectionSharingan,
