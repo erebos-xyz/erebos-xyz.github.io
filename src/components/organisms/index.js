0425390b0d7d9b94c1b9337a82dc3b68e979923b
Angelo Petschnig <32222903+petschna@users.noreply.github.com>
2020-07-02 17:59:47 +0200

    Update src/components/organisms/index.js
    Co-authored-by: Florian Kleber <kleberbaum@erebos.xyz>

--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -1,7 +1,6 @@
 //> Components
 // Import all components to export them for easy access from parent components
 //import FooterGlobal from "./FooterGlobal";
-//import NavbarGlobal from "./NavbarGlobal";
 import NavbarGlobal from "./NavbarGlobal";
 import AboutPage from "./SectionAbout";
 import SectionMotd from "./SectionMotd";
