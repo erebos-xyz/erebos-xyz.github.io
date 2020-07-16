c96121d7823845e870a67207fa245e7811184713
petschna <petschna@edu.htl-villach.at>
2020-07-16 21:14:10 +0200

    Updates

---
+++ precache-manifest.7b2488c2d41548ae646963d808cc17a2.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77b79d9c612a63c74e2c1d1bb3fdbf38",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e1e67ff1dfacd8442b67",
+    "url": "./static/css/2.d56a7b68.chunk.css"
+  },
+  {
+    "revision": "b0ebf817e1a369b11964",
+    "url": "./static/css/main.c5ac15d2.chunk.css"
+  },
+  {
+    "revision": "e1e67ff1dfacd8442b67",
+    "url": "./static/js/2.1ceb7924.chunk.js"
+  },
+  {
+    "revision": "b46eae19120e0343c25f399c83a404d7",
+    "url": "./static/js/2.1ceb7924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0ebf817e1a369b11964",
+    "url": "./static/js/main.0e622fa8.chunk.js"
+  },
+  {
+    "revision": "7b6c2a4e07f335153502",
+    "url": "./static/js/runtime-main.4285ae8f.js"
+  },
+  {
+    "revision": "9136a279c99d1a6915a9b5ae53c98547",
+    "url": "./static/media/React-icon.9136a279.svg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "./static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "./static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "87660d90272ff554a7e186b79043fc85",
+    "url": "./static/media/artjomowitch.87660d90.jpg"
+  },
+  {
+    "revision": "6d1402876c6e146312402e5e11156f66",
+    "url": "./static/media/cloud1.6d140287.png"
+  },
+  {
+    "revision": "d9e56f5bcf1e4d8e73a02b988a793e99",
+    "url": "./static/media/cloud2.d9e56f5b.png"
+  },
+  {
+    "revision": "90dae61784adff78480c878d9cba636b",
+    "url": "./static/media/dark_mountain.90dae617.png"
+  },
+  {
+    "revision": "dee465e2ab7763cdf3f65ecec1d79270",
+    "url": "./static/media/dark_mountain.dee465e2.svg"
+  },
+  {
+    "revision": "007abdac1ed70f72454c4ca17c5b81b4",
+    "url": "./static/media/ezgif.com-crop.007abdac.mov"
+  },
+  {
+    "revision": "27a2380079a4e372d499bf254ec2b76b",
+    "url": "./static/media/f3.27a23800.png"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "./static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "./static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "./static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "./static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "./static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "./static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "./static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "./static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "./static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "./static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "./static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "./static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "./static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "./static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "./static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "39dcca8dc0f8deb04986d56ddcca588e",
+    "url": "./static/media/flaser.39dcca8d.png"
+  },
+  {
+    "revision": "3b322c3dcb03e95be34f5579027ed7ff",
+    "url": "./static/media/kami.3b322c3d.jpg"
+  },
+  {
+    "revision": "a3a590ccf19c20adc92b31c1279cb916",
+    "url": "./static/media/keinbock.a3a590cc.png"
+  },
+  {
+    "revision": "d6c8fcdc4f8fd1c355edab81e3f1c104",
+    "url": "./static/media/member.d6c8fcdc.jpg"
+  },
+  {
+    "revision": "e6f1570e05a95025819ddca09e785ba0",
+    "url": "./static/media/mitsudomoe1.e6f1570e.png"
+  },
+  {
+    "revision": "076ce8bbadfba483798a6d4fb26c49e7",
+    "url": "./static/media/mitsudomoe3.076ce8bb.png"
+  },
+  {
+    "revision": "9d547553e6fc6205c12877263761dd66",
+    "url": "./static/media/moon.9d547553.png"
+  },
+  {
+    "revision": "0c18bf00eee374197999d242c779c661",
+    "url": "./static/media/pink_mountain.0c18bf00.svg"
+  },
+  {
+    "revision": "84a4f32d8a36399c20d0def991d47005",
+    "url": "./static/media/pink_mountain.84a4f32d.png"
+  },
+  {
+    "revision": "08e785fdffc5cd371d16b8110534269c",
+    "url": "./static/media/purple_mountain.08e785fd.png"
+  },
+  {
+    "revision": "68c8d38178d2b8ae1f0e83617825ce81",
+    "url": "./static/media/purple_mountain.68c8d381.svg"
+  },
+  {
+    "revision": "b0b5c219da055fb1ce59b5d90946d492",
+    "url": "./static/media/sebi.b0b5c219.jpg"
+  },
+  {
+    "revision": "ddb7d521890306e47cc709d9183fad95",
+    "url": "./static/media/shinobu.ddb7d521.png"
+  },
+  {
+    "revision": "43c6360bbe4155396ac831fd21bbef7b",
+    "url": "./static/media/star_eule.43c6360b.png"
+  },
+  {
+    "revision": "3133deacc46792a14d10555b9242c25f",
+    "url": "./static/media/star_kroko.3133deac.png"
+  },
+  {
+    "revision": "2214071deab53fb16254a6c2a65c3997",
+    "url": "./static/media/suppi.2214071d.jpg"
+  }
+]);
